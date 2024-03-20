54013177b18e4c7d8d847368c3c9f2a0f02e21a9
bhanu79755 <bhanushreeka2004@gmail.com>
2024-03-20 16:23:43 +0530

    insertion and deletion at front of singly linked list

--- singlyLinkedList.c
+++ singlyLinkedList.c
@@ -97,7 +97,81 @@ void delete_atend()
     }
     count--;
 }
+void insert_atfront()
+{
+    if(first==NULL)
+    {
+        first=temp;
+    }
+    else{
+        temp->next=first;
+        first=temp;
+    }
+}
+void delete_atfront()
+{
+    temp=first;
+    if(first==NULL)
+    {
+        printf("LIST IS EMPTY\n");
+        return;
+    }
+    else if(temp->next==NULL)
+    {
+        printf("%s%s%s%d%lld",temp->usn,temp->name,temp->branch,temp->sem,temp->phno);
+        free(temp);
+        first=NULL;
+    }
+    else{
+        printf("%s%s%s%d%lld",temp->usn,temp->name,temp->branch,temp->sem,temp->phno);
+        first=temp->next;
+        free(temp);
+    }
+    count--;
+}
+void main()
+{
+    int choice,n,i;
+    while(1)
+    {
+        printf("MENU\n");
+        printf("1:TO CREATE A SLL OF N STUDENTS DATA BY USING FRONT INSERTION");
+        printf("2:DISPLAY THE STATUS OF SLL AND COUNT THE NUMBER OF NODES IN IT");
+        printf("3:INSERTION AT END OF SLL");
+        printf("4:DELETION AT END OF SLL");
+        printf("INSERTION AT FRONT OF SLL(DEMONSTARTION OF STACK)");
+        printf("DELTIONION AT FRONT OF SLL(DEMONSTARTION OF STACK)");
+        printf("7:EXIT\n\n");
+        printf("ENTER YOUR CHOICE");
+        scanf("%d",&choice)
+        {
+            case 1:printf("\nENTER NUMBER OF STUDENTS:");
+                   scanf("%d",&n);
+                   for(i=0;i<n;i++)
+                 {
+                    readdata();
+                    create();
+                 }
+                    break;
+            case 2:display();
+                   break;
+            case 3:readdata();
+                   insert_atend();break;
+            case 4:delete_atend()
+                   break;
+            case 5:readdata();
+                   insert_atfront();
+                   break;
+            case 6:delete_atfront()
+                   break;
+            case 7:exit(0);
+            default:printf("WRONG CHOICE\n");
 
+        }
+
+
+    }
+}
 
 
 
